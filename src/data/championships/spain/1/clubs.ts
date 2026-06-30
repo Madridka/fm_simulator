bae986d281e4d337ff930c7624a7251ad52bc76d
Сербин Кирилл <serbin_kirill1807@mail.ru>
2026-06-30 12:38:51 +0700

    fix: исправил 4 дивизиона англии

--- src/data/championships/spain/1/clubs.ts
+++ src/data/championships/spain/1/clubs.ts
@@ -1,4 +1,4 @@
-import { realMadridProfile } from '@/data/clubDatabase/spain/LaLiga/real_madrid'
+import { realMadridProfile } from '@/data/clubDatabase/spain/LaLiga/real-madrid'
 import { SpainLeagueId } from '@/data/clubs/enums'
 import type { ClubProfile } from '@/data/clubs/types'
 
